73757b8db344ed9c68e2b4819a37d09b93322ae5
anncannbann <anncannbann@gmail.com>
2024-01-25 19:23:11 +0530

    Day 3 Pizza bill retry

--- 100 days/pizza.py
+++ 100 days/pizza.py
@@ -7,24 +7,18 @@ extra_cheese = input() # Do you want extra cheese? Y or N
 bill =0
 
 if size =='S':
-  bill =15
-  if add_pepperoni =='Y':
-      bill += 2
-  if extra_cheese =='Y':
-      bill+=1
+    bill+=15
+elif size =='M':
+    bill +=20
+else:
+   bill +=25
 
-if size =='M':
-  bill =20
-  if add_pepperoni =='Y':
-      bill += 3
-  if extra_cheese =='Y':
-      bill+=1
-
-if size =='L':
-  bill =25
-  if add_pepperoni =='Y':
-      bill +=3
-  if extra_cheese =='Y':
+if add_pepperoni =='Y':
+    if size=='S':
+           bill += 2
+    else:
+         bill +=3
+if extra_cheese =='Y':
       bill+=1
 
 
