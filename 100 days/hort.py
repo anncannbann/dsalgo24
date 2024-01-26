31dae98bcb1fe7a1b42cadaee86313548653b14e
anncannbann <anncannbann@gmail.com>
2024-01-26 22:41:13 +0530

    heads or tails using Random module

---
+++ 100 days/hort.py
@@ -0,0 +1,20 @@
+#heads or tails random
+import random
+import mymodu
+
+
+
+#print(mymodu.pi)
+#x = print(10* random.random())
+
+
+Heads = 1
+Tails = 0
+
+toss = random.randint(0,2)
+
+if toss ==1:
+  print('Heads')
+
+else:
+  print('Tails')
