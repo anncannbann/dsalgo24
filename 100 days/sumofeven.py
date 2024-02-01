dd47ab776b0496f12aa8471726357fe2e02c7201
anncannbann <anncannbann@gmail.com>
2024-02-01 14:19:18 +0530

    Day 5: Sum of even optimise

--- 100 days/sumofeven.py
+++ 100 days/sumofeven.py
@@ -4,8 +4,7 @@ target = int(input()) # Enter a number between 0 and 1000
 # Write your code here 👇
 
 temp =0
-for i in range(1,target+1):
-  if i%2 ==0:
+for number in range(2, target + 1, 2):
     #print(i)
     temp+=i
 
