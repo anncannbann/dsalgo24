fe2c79e7c069aaec260cf2e161fa6acfe57f6743
anncannbann <anncannbann@gmail.com>
2024-02-06 23:19:31 +0530

    day8 functions decode

--- 100 days/day8:caeser_cypher/caeser_decode.py
+++ 100 days/day8:caeser_cypher/caeser_decode.py
@@ -5,9 +5,7 @@ alphabet = [
     't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+
 
 enc = []
 
@@ -47,5 +45,19 @@ def decrypt(enc, shift):
 #print output: "The decoded text is hello"
 
 #TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-encrypt(text, shift)
-decrypt(enc, shift)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+
+if direction =='encode' or direction =='decode':
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+else:
+  print('Enter a valid choice please.')
+
+if direction == "encode":
+  encrypt(text, shift)
+elif direction == "decode":
+  decrypt(enc, shift)
+
+
+
