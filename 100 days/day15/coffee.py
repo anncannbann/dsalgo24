29059543fdef716f109b949e94741375ae599e2c
anncannbann <anncannbann@gmail.com>
2024-02-21 18:06:54 +0530

    starter code

---
+++ 100 days/day15/coffee.py
@@ -0,0 +1,49 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+machine = 'on'
+
+while machine:
+    req = input('would you like a latte/espresso/cappuccino?')
+
+    if req == 'latte':
+        continue
+    elif req == 'espresso':
+        continue
+    elif req == 'cappuccino':
+        continue
+    
+    elif req == 'report':
+        print(resources)
+    elif req == 'off':
+        exit()
+
