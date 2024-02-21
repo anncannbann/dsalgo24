b063f89732422b0eff7a8e63404c09839bf94717
anncannbann <anncannbann@gmail.com>
2024-02-22 01:39:02 +0530

    added profit

--- 100 days/day15/coffee.py
+++ 100 days/day15/coffee.py
@@ -23,7 +23,7 @@ MENU = {
         "cost": 3.0,
     }
 }
-
+profit = 0
 resources = {
 
     "water": 300,
@@ -57,18 +57,18 @@ def coins(res):
     x = MENU[res]['cost']
 
     total = float((q*0.25)+(d*0.10)+(n * 0.05)+(p * 0.01))
+    change = round(total-x,2)
     print(total)
 
     if total < x:
         return "Sorry that's not enough money. Money refunded."
     elif total > x:
         print(make_coffee(res))
-        return f"Here is ${round(total-x,2)} dollars in change."
+        global profit
+        profit += x
+        return f"Here is ${change} dollars in change."
     else:
         return "No change need, exact amount given."
-
-
-
 
 machine = True
 
@@ -77,6 +77,7 @@ while machine:
     req = input('would you like a latte/espresso/cappuccino?')
     if req == 'report':
         print(resources)
+        print(profit)
     else:
 
         res = check_resources(req)
