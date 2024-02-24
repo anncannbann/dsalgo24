9fcb09a1ba256074c7d03c43f6eaa4d587c39b4b
anncannbann <anncannbann@gmail.com>
2024-02-25 02:27:04 +0530

    Pretty Tables crete objects

--- 100 days/day16/OOP.py
+++ 100 days/day16/OOP.py
@@ -1,16 +1,22 @@
-from turtle import Turtle, Screen
-
-print('Making a turtle')
-timmy = Turtle()
-print(timmy)
-timmy.shape('turtle')
-my_screen = Screen()
-timmy.color('DeepPink')
-timmy.forward(100)
-# timmy.left(120)
+# from turtle import Turtle, Screen
+#
+# print('Making a turtle')
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape('turtle')
+# my_screen = Screen()
+# timmy.color('DeepPink')
 # timmy.forward(100)
-# timmy.left(120)
-# timmy.forward(100)
-print(my_screen.canvheight)
+# # timmy.left(120)
+# # timmy.forward(100)
+# # timmy.left(120)
+# # timmy.forward(100)
+# print(my_screen.candlelight)
+# my_screen.exitonclick()
 
-my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+mytable = PrettyTable()
+
+print(mytable)
