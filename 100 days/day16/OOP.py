12d7d7fa00fe468be4793e4bd3ced45d6f58bbc6
anncannbann <anncannbann@gmail.com>
2024-02-25 02:24:13 +0530

    Pretty Tbles

--- 100 days/day16/OOP.py
+++ 100 days/day16/OOP.py
@@ -1,5 +1,16 @@
-import turtle
+from turtle import Turtle, Screen
 
 print('Making a turtle')
-timmy = turtle.Turtle()
+timmy = Turtle()
 print(timmy)
+timmy.shape('turtle')
+my_screen = Screen()
+timmy.color('DeepPink')
+timmy.forward(100)
+# timmy.left(120)
+# timmy.forward(100)
+# timmy.left(120)
+# timmy.forward(100)
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
