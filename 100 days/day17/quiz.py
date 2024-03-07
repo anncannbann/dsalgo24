9b7481fc85e45a5983c821667be225feb7607241
anncannbann <anncannbann@gmail.com>
2024-03-08 00:00:09 +0530

    Quiz.py

--- 100 days/day17/quiz.py
+++ 100 days/day17/quiz.py
@@ -1,2 +1,12 @@
 from data import question_data
+from question_model import Question
 
+
+question_bank =[]
+
+
+for questions in question_data:
+    q = Question(questions["text"], questions["answer"])
+    question_bank.append(q)
+
+print(question_bank)
