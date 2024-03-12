583ccbbd259ec29530ccf5ed8bd0114498bbbaec
anncannbann <anncannbann@gmail.com>
2024-03-12 16:38:14 +0530

    Quiz Brain quiz questions pull up

--- 100 days/day17/quiz.py
+++ 100 days/day17/quiz.py
@@ -1,6 +1,6 @@
 from data import question_data
 from question_model import Question
-
+from quiz_brain import QuizBrain
 
 question_bank =[]
 
@@ -10,3 +10,7 @@ for questions in question_data:
     question_bank.append(q)
 
 print(question_bank)
+
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
--- 100 days/day17/quiz_brain.py
+++ 100 days/day17/quiz_brain.py
@@ -1,4 +1,10 @@
+class QuizBrain:
 
+    def __init__(self, q_list):
+        self.question_no = 0
+        self.question_lst = q_list
 
-class Quiz:
-    def __init__(self):
+    def next_question(self):
+        current_question = self.question_lst[self.question_no]
+        self.question_no+=1
+        input(f"Q.{self.question_no}{current_question.text} (True / False)")
