e93bcb5044a3f040a4d99a1faf15a95ceb1afd5c
anncannbann <anncannbann@gmail.com>
2024-03-15 19:39:41 +0530

    Turtle make a square

--- 100 days/day18/main.py
+++ 100 days/day18/main.py
@@ -1,24 +1,14 @@
-from turtle import Turtle,Screen
-
+from turtle import Turtle, Screen
 
 timmy = Turtle()
 
-timmy.forward(100)
+timmy.shape("turtle")
+timmy.color('black')
+timmy.fillcolor('BlueViolet')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for i in range(0, 4):
+    timmy.forward(100)
+    timmy.left(90)
 
 screen = Screen()
 screen.exitonclick()
