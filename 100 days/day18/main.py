94fdf892155576b957e636b7e483ca071830502d
anncannbann <anncannbann@gmail.com>
2024-03-16 22:53:17 +0530

    Turtle make a dashed line

--- 100 days/day18/main.py
+++ 100 days/day18/main.py
@@ -5,10 +5,19 @@ timmy = Turtle()
 timmy.shape("turtle")
 timmy.color('black')
 timmy.fillcolor('BlueViolet')
-
+# to draw a square
+'''
 for i in range(0, 4):
     timmy.forward(100)
     timmy.left(90)
+'''
+
+for i in range(0,50):
+    timmy.forward(5)
+    timmy.penup()
+    timmy.forward(5)
+    timmy.pendown()
+
 
 screen = Screen()
 screen.exitonclick()
