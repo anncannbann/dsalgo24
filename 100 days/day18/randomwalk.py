7c2b0608d03b968a66e5dbb4eb0e6c7600849586
anncannbann <anncannbann@gmail.com>
2024-03-17 12:23:24 +0530

    Turtle random path done

--- 100 days/day18/randomwalk.py
+++ 100 days/day18/randomwalk.py
@@ -3,22 +3,24 @@ import random
 
 t = Turtle()
 
-t.pensize(5)
+t.pensize(3)
 t.shape("turtle")
 t.color('black')
 t.fillcolor('BlueViolet')
+t.speed('fastest')
+# choices = {'steps': ['t.forward','t.backward','t.left','t.right']}
+angles = [0, 90, 180, 270]
 
-choices = ['forward()','backward()','left()','right()']
 
 def walk():
-
     x = 0
 
-    while x<20:
+    while x < 100:
         t.pencolor(random.random(), random.random(), random.random())
-        move = random.choice(choices)
-        move()
-        x+=1
+        choice = random.choice(angles)
+        t.right(choice)
+        t.forward(random.randint(5, 50))
+        x += 1
 
 
 walk()
