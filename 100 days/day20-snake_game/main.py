4f778d1a5df3c6822fde46360ebf039e22e7cd47
anncannbann <anncannbann@gmail.com>
2024-03-18 02:38:02 +0530

    snake move forward

--- 100 days/day20-snake_game/main.py
+++ 100 days/day20-snake_game/main.py
@@ -2,22 +2,27 @@ from turtle import Turtle, Screen
 import random
 
 s = Screen()
-
-snake =[]
-
-x = [0,20,40]
-
-for i in range(0,3):
-    t = Turtle(shape='square')
-    t.goto(x[i],0)
-    t.color('white')
-
-
-
-
-
-s.setup(600,600)
+s.setup(600, 600)
 s.bgcolor('black')
 s.title('Snake Game')
+s.tracer(0)
+
+snake = []
+
+x = [0, -20, -40]
+
+for i in range(0, 3):
+    t = Turtle(shape='square')
+    t.penup()
+    t.goto(x[i], 0)
+    t.color('white', 'purple')
+    snake.append(t)
+
+run_snake = True
+
+while run_snake:
+    for snack in snake:
+        snack.forward(10)
+
 
 s.exitonclick()
