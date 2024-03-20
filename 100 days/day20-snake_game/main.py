0c134182ee7fbd2c8bd38508f6a18be8abcab810
anncannbann <anncannbann@gmail.com>
2024-03-20 23:53:43 +0530

    Refactor and make seperate Snake class

--- 100 days/day20-snake_game/main.py
+++ 100 days/day20-snake_game/main.py
@@ -26,10 +26,10 @@ while run_snake:
     s.update()
     time.sleep(0.1)
 
-    for snacks in range(len(snake)-1,0,-1):
-        new_x = snake[snacks -1].xcor()
+    for snacks in range(len(snake) - 1, 0, -1):
+        new_x = snake[snacks - 1].xcor()
         new_y = snake[snacks - 1].ycor()
-        snake[snacks].goto(new_x,new_y)
+        snake[snacks].goto(new_x, new_y)
     snake[0].forward(20)
 
 s.exitonclick()
