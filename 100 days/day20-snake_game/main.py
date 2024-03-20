c7cd9f695198adc21d9a548b8bb886d8b9f23bf1
anncannbann <anncannbann@gmail.com>
2024-03-20 23:49:21 +0530

    snake move forward dynamically

--- 100 days/day20-snake_game/main.py
+++ 100 days/day20-snake_game/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import random
+import time
 
 s = Screen()
 s.setup(600, 600)
@@ -18,11 +19,17 @@ for i in range(0, 3):
     t.color('white', 'purple')
     snake.append(t)
 
+s.update()
 run_snake = True
 
 while run_snake:
-    for snack in snake:
-        snack.forward(10)
+    s.update()
+    time.sleep(0.1)
 
+    for snacks in range(len(snake)-1,0,-1):
+        new_x = snake[snacks -1].xcor()
+        new_y = snake[snacks - 1].ycor()
+        snake[snacks].goto(new_x,new_y)
+    snake[0].forward(20)
 
 s.exitonclick()
